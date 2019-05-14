bca8b0f7de6816e549910e43983284287d3b4a1b
Michael Tabor <mike@miketabor.com>
2019-05-14 09:10:13 -0400

    Add files via upload

---
+++ database.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+################################################
+#
+# Backup all MySQL databases in separate files and compress each file.
+# NOTES:
+# - MySQL and pigz must be installed on the system
+# - Requires write permission in the destination folder
+# - Excludes MySQL admin tables ('mysql',information_schema','performance_schema')
+#
+################################################
+
+##### VARIABLES
+# MySQL User
+USER='root'
+# MySQL Password
+PASSWORD='MySQL_ROOT_PASSWORD'
+# Backup Directory - WITH TAILING SLASH IF PATH OTHER THEN '.'!
+OUTPUT="/home/backups/mysql"
+BUCKET="NAME_OF_S3_BUCKET"
+ 
+##### EXECUTE THE DB BACKUP
+TIMESTAMP=`date +%Y%m%d_%H`;
+OUTPUTDEST=$OUTPUT;
+echo "Starting MySQL Backup";
+echo `date`;
+databases=`mysql --user=$USER --password=$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
+for db in $databases; do
+    if [[ "$db" != "information_schema" ]] && [[ "$db" != _* ]] && [[ "$db" != "mysql" ]] && [[ "$db" != "performance_schema" ]] ; then
+        echo "Dumping database: $db"
+        mysqldump --single-transaction --routines --triggers --user=$USER --password=$PASSWORD --databases $db > $OUTPUTDEST/dbbackup-$TIMESTAMP-$db.sql
+        pigz $OUTPUTDEST/dbbackup-$TIMESTAMP-$db.sql
+    fi
+done
+/usr/local/bin/aws --only-show-errors s3 sync $OUTPUTDEST s3://$BUCKET/`date +%Y`/`date +%m`/`date +%d`/
+rm -rf /home/backups/mysql/dbbackup-*
+echo "Finished MySQL Backup";
+echo `date`;
---
+++ files.sh
@@ -0,0 +1,21 @@
+################################################
+#
+# Backup all files in the nginx home directory.
+# NOTES:
+# - MySQL and gzip must be installed on the system
+#
+################################################
+
+##### VARIABLES
+OUTPUT="/home/backups/files"
+ 
+##### EXECUTE THE FILES BACKUP
+TIMESTAMP=`date +%Y%m%d_%H`;
+OUTPUTDEST=$OUTPUT;
+echo "Starting Files Backup";
+cd /home/nginx/domains/
+tar -czf $OUTPUTDEST/filesbackup-SERVER_HOST_NAME-$TIMESTAMP-$db.tar *
+/usr/local/bin/aws --only-show-errors s3 cp $OUTPUTDEST/filesbackup-SERVER_HOST_NAME-$TIMESTAMP-$db.tar s3://AWS_BUCKET_NAME/
+rm -f $OUTPUTDEST/filesbackup-*
+echo "Finished Files Backup";
+echo `date`;
