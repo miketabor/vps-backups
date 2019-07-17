d6bb904fb04c6f98c19ce0dd76927ecfd2aecbee
Michael Tabor <mike@miketabor.com>
2019-07-17 07:40:00 -0400

    Update files.sh

--- files.sh
+++ files.sh
@@ -2,7 +2,7 @@
 #
 # Backup all files in the nginx home directory.
 # NOTES:
-# - MySQL and gzip must be installed on the system
+# - gzip must be installed on the system
 #
 ################################################
 
@@ -14,8 +14,8 @@ TIMESTAMP=`date +%Y%m%d_%H`;
 OUTPUTDEST=$OUTPUT;
 echo "Starting Files Backup";
 cd /home/nginx/domains/
-tar -czf $OUTPUTDEST/filesbackup-SERVER_HOST_NAME-$TIMESTAMP-$db.tar *
-/usr/local/bin/aws --only-show-errors s3 cp $OUTPUTDEST/filesbackup-SERVER_HOST_NAME-$TIMESTAMP-$db.tar s3://AWS_BUCKET_NAME/
+tar -czf $OUTPUTDEST/filesbackup-SERVER_HOST_NAME-$TIMESTAMP.tar *
+/usr/local/bin/aws --only-show-errors s3 cp $OUTPUTDEST/filesbackup-SERVER_HOST_NAME-$TIMESTAMP.tar s3://AWS_BUCKET_NAME/
 rm -f $OUTPUTDEST/filesbackup-*
 echo "Finished Files Backup";
 echo `date`;
