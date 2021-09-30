61fd83e4a7007d72efd295c03213b9eb4b27dbb4
Michael Tabor <mike@miketabor.com>
2021-09-30 08:25:52 -0400

    Update files.sh

--- files.sh
+++ files.sh
@@ -15,7 +15,7 @@ OUTPUTDEST=$OUTPUT;
 echo "Starting Files Backup";
 cd /home/nginx/domains/
 tar -czf $OUTPUTDEST/filesbackup-SERVER_HOST_NAME-$TIMESTAMP.tar *
-/usr/local/bin/aws --only-show-errors s3 cp $OUTPUTDEST/filesbackup-SERVER_HOST_NAME-$TIMESTAMP.tar s3://AWS_BUCKET_NAME/
+aws --only-show-errors s3 cp $OUTPUTDEST/filesbackup-SERVER_HOST_NAME-$TIMESTAMP.tar s3://AWS_BUCKET_NAME/
 rm -f $OUTPUTDEST/filesbackup-*
 echo "Finished Files Backup";
 echo `date`;
